8c6d71700f14d543648bc4632d05b493f4cbb5b1
luna <selenologist@users.noreply.github.com>
2020-02-24 06:21:43 +1100

    Add summary generator

--- htmlgen.py
+++ htmlgen.py
@@ -1,5 +1,7 @@
 import sys
 import datetime
+import cedict
+from cedict2pinyin import add_accents
 
 #########################
 # HTML table generation #
@@ -86,5 +88,113 @@ a {{
     f.write("</table></body></html>")
 
     return True
+
+# defs is cedict.load() format
+def summary(words, defs,
+        f=open("/dev/stdout", "w"),
+        title="Randomly Generated Summary"):
+
+    # generate start of HTML page including CSS
+    f.write(\
+"""<!DOCTYPE html>
+<html lang="zh">
+<head><title>{}</title>
+<style>
+table {{
+    width: 100%;
+    border-spacing: 0px;
+    border-collapse: collapse;
+    margin: 0px;
+    padding: 0px;
+    page-break-inside:auto;
+}}
+tr {{
+    page-break-inside:avoid;
+    page-break-after:auto;
+}}
+td {{
+    border: 1px solid black;
+    text-align: center;
+    padding: 0px;
+    margin: 0px;
+    vertical-align: top;
+}}
+th {{
+    font-size: 150%;
+}}
+
+.c {{
+    font-size: 250%;
+    font-family: "FandolFang R", fangsong, serif;
+}}
+
+.p {{
+    font-size: 150%;
+}}
+.p a {{
+    display: block; 
+    min-width: max-content;
+}}
+
+a {{
+    color: black;
+    text-decoration: none;
+}}
+</style>
+</head>
+<body><table>
+<tr><th>词</th><th>拼音</th><th>定义</th></tr>""".format(title))
+   
+    # generate rows for each character
+    for word in words:
+        row = '<tr><td class="c">'
+        for c in word:
+            row += '<a href="https://zici.info/decomp/#{}N">{}</a>'.format(hex(ord(c))[2:], c)
+        row += "</td>"
+        
+        py, definition = '?', None
+        if word in defs:
+            for entry in defs[word]:
+                py, definition = entry[cedict.PINYIN_INDEX], entry[cedict.DEF_INDEX]
+                
+                if definition.startswith("variant"):
+                    continue # skip variant definitions
+
+                # split slashes in definitions over lines, skipping 'variant' definitions again
+                definitions =\
+                    filter(lambda d: not d.startswith("variant"), definition.split(sep='/'))
+                definition = ""
+                line = ""
+                LINE_CUTOFF = 80
+                for d in definitions:
+                    if len(line) + len(d) + 3 < LINE_CUTOFF:
+                        if len(line):
+                            line += " / "
+                        line += d
+                    else:
+                        if len(definition):
+                            definition += "<br>"
+                        definition += line
+                        line = ""
+                if len(line):
+                    if len(definition):
+                        definition += "<br>"
+                    definition += line
+
+                py = "<br>".join(map(add_accents, py.split()))
+                
+                # clicking the pinyin shows the yellowbridge dictionary entry
+                row += '<td class="p"><a href="https://www.yellowbridge.com/chinese/dictionary.php?word=={}">{}</a></td>'\
+                    .format(word, py)
+                row += '<td>{}</td></tr>'.format(definition)
+                f.write(row)
+
+                row = "<tr><td>↑</td>"
+        else:
+            sys.stderr.write("no pinyin found for {}\n".format(word))
+    # finally terminate the HTML page
+    f.write("</table></body></html>")
+
+    return False
 
 
